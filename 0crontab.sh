21702828f6620b43b0434df2551b34d4cf34dc72
HSD-FR <github@hsd.me>
2026-03-23 21:45:35 +0100

    Update crontab script to run 1min and 5min scripts

--- 0crontab.sh
+++ 0crontab.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ###############################################
 ###                                         ###
-###     Crontab - exec every 5min v1.O      ###
-###     2025-08-30   StillTRue(c)           ###
+###     Crontab - exec 1min & 5min v1.1     ###
+###     2026-03-23   StillTRue(c)           ###
 ###                                         ###
 ###############################################
 
-# Script runner to execute all *5min.sh scripts
-# Put this in cron every 5 minutes
+# Script runner to execute all *5min.sh and all *1min.sh scripts
+# Put this in cron every 1 minute
 
 SCRIPT_DIR="/mnt/data"
 LOG_FILE="/mnt/data/log/crontab.log"
@@ -15,13 +15,29 @@ LOG_FILE="/mnt/data/log/crontab.log"
 # Create log file if not exists
 touch "$LOG_FILE"
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] Running all *5min.sh scripts" >> "$LOG_FILE"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Running scheduled scripts" >> "$LOG_FILE"
 
-for script in "$SCRIPT_DIR"/*-5min.sh; do
-    if [ -x "$script" ]; then
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] Executing $script" >> "$LOG_FILE"
-        "$script" >> "$LOG_FILE" 2>&1
-    else
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] Skipping $script (not executable)" >> "$LOG_FILE"
-    fi
-done
+# Function to run scripts matching a pattern
+run_scripts() {
+    local pattern=$1
+    for script in "$SCRIPT_DIR"/*"$pattern"; do
+        if [ -f "$script" ]; then
+            if [ -x "$script" ]; then
+                echo "[$(date '+%Y-%m-%d %H:%M:%S')] Executing $script" >> "$LOG_FILE"
+                "$script" >> "$LOG_FILE" 2>&1
+            else
+                echo "[$(date '+%Y-%m-%d %H:%M:%S')] Skipping $script (not executable)" >> "$LOG_FILE"
+            fi
+        fi
+    done
+}
+
+MINUTE=$(date +%M)
+
+# Run 1min scripts
+run_scripts "-1min.sh"
+
+# Run 5min scripts only every 5 minutes
+if ((10#$MINUTE % 5 == 0)); then
+    run_scripts "-5min.sh"
+fi
